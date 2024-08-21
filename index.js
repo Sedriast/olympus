6f57c03b04169712ab3e5e461574440a03a30ac8
exSED <sedriast@gmail.com>
2024-08-21 12:16:30 -0500

    Rebuild

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
+import App from './src/components/App';
+import { name as appName } from './app.json';
 
-import App from './src/components/App/App';
-
-registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => App);
